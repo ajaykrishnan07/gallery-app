89a1b643f2e07f2dd44a18e499990ed7e9b23ae9
ajaykrishnan07 <ajkrish2002@gmail.com>
2023-08-24 01:06:42 +0530

    Search Box pending
    Need to add the search box for image search .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import { ImageCard } from "./components/ImageCard";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${
+        import.meta.env.VITE_APP_PIXABAY_API_KEY
+      }&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
-    <div className="max-w-sm  rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl text-cyan-500">
-          {" "}
-          Photographed by Ajay{" "}
+    <div className="container mx-auto">
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
         </div>
-      </div>
+      )}
     </div>
   );
 }
---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export const ImageCard = ({ image }) => {
+  const tags = image.tags.split(",");
+
+  return (
+    <div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img src={image.webformatURL} alt="" className="w-full" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl text-cyan-500 mb-2">
+            Photographed by {image.user}
+          </div>
+          <ul>
+            <li>
+              <strong>Views: </strong>
+              {image.views}
+            </li>
+            <li>
+              <strong>Downloads: </strong>
+              {image.downloads}
+            </li>
+            <li>
+              <strong>Likes: </strong>
+              {image.likes}
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-grey-700 mr-2"
+            >
+              #{tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
